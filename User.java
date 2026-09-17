002857e39ba55b2506642ad1859c5067b5ba816e
avishi25bai10071 <avishi.25bai10071@vitbhopal.ac.in>
2026-09-17 15:48:09 +0530

    Add files via upload

---
+++ User.java
@@ -0,0 +1,45 @@
+package library;
+
+public class User {
+
+    private String userId;
+    private String name;
+    private String email;
+
+    public User(String userId, String name, String email) {
+        this.userId = userId;
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void displayUser() {
+        System.out.println("User ID: " + userId);
+        System.out.println("Name: " + name);
+        System.out.println("Email: " + email);
+    }
+
+    @Override
+    public String toString() {
+        return userId + " | " + name + " | " + email;
+    }
+}
